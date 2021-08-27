f4e70575a6335bb0cecdc72fea76cf4a61d3d2b0
sergey <samael.env@gmail.com>
2021-08-27 20:41:03 +0300

    Готово всё и докер

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['195.133.48.21']
+ALLOWED_HOSTS = []
 
 # Application definition
 
