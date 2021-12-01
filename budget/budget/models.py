9d5ebb54f762f268a2bd34b38e19058f61e9c64a
sergey <samael.env@gmail.com>
2021-12-01 14:04:13 +0300

    добавил get_or_create, исправил редактирование плановых

--- budget/budget/models.py
+++ budget/budget/models.py
@@ -8,16 +8,16 @@ class Header(models.Model):
     def __str__(self):
         return self.name
 
-    @staticmethod
-    def add_header_to_transaction(header):
-        header = header.lstrip().rstrip().capitalize()
-        headers_list = Header.objects.all()
-        for el in headers_list:
-            if el.name == header:
-                return el.pk
-        new_header = Header(name=header)
-        new_header.save()
-        return new_header.pk
+    # @staticmethod
+    # def add_header_to_transaction(header):
+    #     header = header.lstrip().rstrip().capitalize()
+    #     headers_list = Header.objects.all()
+    #     for el in headers_list:
+    #         if el.name == header:
+    #             return el.pk
+    #     new_header = Header(name=header)
+    #     new_header.save()
+    #     return new_header.pk
 
 
 class Category(models.Model):
@@ -26,16 +26,16 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    @staticmethod
-    def add_category_to_transaction(category):
-        category = category.lstrip().rstrip().capitalize()
-        categories_list = Category.objects.all()
-        for el in categories_list:
-            if el.name == category:
-                return el.pk
-        new_category = Category(name=category)
-        new_category.save()
-        return new_category.pk
+    # @staticmethod
+    # def add_category_to_transaction(category):
+    #     category = category.lstrip().rstrip().capitalize()
+    #     categories_list = Category.objects.all()
+    #     for el in categories_list:
+    #         if el.name == category:
+    #             return el.pk
+    #     new_category = Category(name=category)
+    #     new_category.save()
+    #     return new_category.pk
 
 
 class Subcategory(models.Model):
@@ -44,16 +44,16 @@ class Subcategory(models.Model):
     def __str__(self):
         return self.name
 
-    @staticmethod
-    def add_subcategory_to_transaction(subcategory):
-        subcategory = subcategory.lstrip().rstrip().capitalize()
-        subcategories_list = Subcategory.objects.all()
-        for el in subcategories_list:
-            if el.name == subcategory:
-                return el.pk
-        new_subcategory = Subcategory(name=subcategory)
-        new_subcategory.save()
-        return new_subcategory.pk
+    # @staticmethod
+    # def add_subcategory_to_transaction(subcategory):
+    #     subcategory = subcategory.lstrip().rstrip().capitalize()
+    #     subcategories_list = Subcategory.objects.all()
+    #     for el in subcategories_list:
+    #         if el.name == subcategory:
+    #             return el.pk
+    #     new_subcategory = Subcategory(name=subcategory)
+    #     new_subcategory.save()
+    #     return new_subcategory.pk
 
 
 class BudgetPeriod(models.Model):
