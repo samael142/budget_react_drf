9219d481567bf4dd82fa5e74fcd42dde1d673c2a
Sergey F <samael.env@gmail.com>
2022-11-11 11:23:38 +0300

    Added shown transfers in account details

--- frontend/src/components/TransactionItem.jsx
+++ frontend/src/components/TransactionItem.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { DateConvert } from "./utils/utils";
+import { useParams } from 'react-router-dom';
 
 const TransactionItem = (props) => {
+
+    const params = useParams()
+
     if (!props.transaction.transfer_id) {
         return (
             <Link to={`/transaction/${props.transaction.id}`}
@@ -10,7 +14,9 @@ const TransactionItem = (props) => {
                 <div className="tr__box__row">
                     <div><b>{props.transaction.header ? props.transaction.header.name : ""}</b>
                     </div>
-                    <div className={"num " + (props.transaction.operation_summ > 0 ? "green__color" : "red__color")}>{parseFloat(props.transaction.operation_summ).toLocaleString("ru",{minimumFractionDigits: 2})}</div>
+                    <div className={"num " + (props.transaction.operation_summ > 0 ? "green__color" : "red__color")}>
+                        {parseFloat(props.transaction.operation_summ).toLocaleString("ru", { minimumFractionDigits: 2 })}
+                    </div>
                 </div>
                 <div className="tr__box__row">
                     <div>{props.transaction.category ? props.transaction.category.name : ""}</div>
@@ -24,20 +30,39 @@ const TransactionItem = (props) => {
         );
     } else {
         return (
-            props.transaction.operation_summ > 0 ?
-            <Link to={`/transfer/${props.transaction.transfer_id}`}
-                className="transition__item tr__box transfer__color">
-                <div className="tr__box__row">
-                    <div><b>{props.transaction.comment}</b>
+            params.accountId
+                ?
+                <Link to={`/transfer/${props.transaction.transfer_id}`}
+                    className="transition__item tr__box transfer__color">
+                    <div className="tr__box__row">
+                        <div><b>{props.transaction.comment}</b>
+                        </div>
+                        <div className={(props.transaction.operation_summ > 0 ? "green__color" : "red__color")}>
+                            {parseFloat(props.transaction.operation_summ).toLocaleString("ru", { minimumFractionDigits: 2 })}
+                        </div>
                     </div>
-                    <div className="black__color">{parseFloat(props.transaction.operation_summ).toLocaleString("ru",{minimumFractionDigits: 2})}</div>
-                </div>
-                <div className="tr__box__row">
-                    <div>Перевод</div>
-                    <div>{DateConvert(props.transaction.operation_date)}</div>
-                </div>
-            </Link>
-            : <></>
+                    <div className="tr__box__row">
+                        <div>Перевод</div>
+                        <div>{DateConvert(props.transaction.operation_date)}</div>
+                    </div>
+                </Link>
+                :
+                props.transaction.operation_summ > 0 ?
+                    <Link to={`/transfer/${props.transaction.transfer_id}`}
+                        className="transition__item tr__box transfer__color">
+                        <div className="tr__box__row">
+                            <div><b>{props.transaction.comment}</b>
+                            </div>
+                            <div className="black__color">
+                                {parseFloat(props.transaction.operation_summ).toLocaleString("ru", { minimumFractionDigits: 2 })}
+                            </div>
+                        </div>
+                        <div className="tr__box__row">
+                            <div>Перевод</div>
+                            <div>{DateConvert(props.transaction.operation_date)}</div>
+                        </div>
+                    </Link>
+                    : <></>
         )
     }
 };
